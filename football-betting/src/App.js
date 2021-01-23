f45fa9a473ac98a36b7adfb45f1b33a8363b5a22
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-01-24 01:24:08 +0200

    Update App.js

--- football-betting/src/App.js
+++ football-betting/src/App.js
@@ -2,23 +2,47 @@ import Board from './containers/Board/Board';
 import { Route, Switch } from "react-router-dom";
 import Navs from './components/UI/Navbar/Navs'
 import { Component } from 'react';
+import {connect} from 'react-redux'
 import classes from './App.module.css';
 import AcceptACard from './containers/CardPayments/AcceptACard/AcceptACard';
 import ResultPage from './containers/Board/ResultPage';
+import * as actions from './store/actions/index'
+import Landing from './components/UI/LandingPage/landing';
 
 class  App extends Component {
+  
   render(){
    
     return(<div className={classes.App}>
-    <div className= {classes.Navs}><Navs /></div>
+    <div className= {classes.Navs}><Navs funds={this.props.funds}
+     showFunds={this.props.showFunds} firstName={'Emmanuel'} 
+    toggleShowFunds={this.props.onToggleShowFunds}/></div>
     <Switch>
       <Route path= "/payment" component ={AcceptACard} />
       <Route path= "/results" component ={ResultPage} />
-      <Route path= "/" component ={Board} />
+      <Route path= "/home" component ={Board} />
+      <Route path= "/" component ={Landing} />
     </Switch>
   
    </div>);
   }
 }
+const mapstateToProps = (state) => {
+  return {
+      
+    showFunds: state.board.showFunds,
+    
+    funds: state.board.funds,
+    
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+    onToggleShowFunds: () => dispatch(actions.toggleShowFunds()),
+    
+  };
+};
 
-export default App;
+export default connect(mapstateToProps, mapDispatchToProps)(App);
+
