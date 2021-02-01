86b0e7aa6b4b75cf32840f10652099a102bf5a23
lordshegz <lordshegz@gmail.com>
2021-02-01 19:33:10 +0000

    getting there

--- football-betting/src/store/reducers/login.js
+++ football-betting/src/store/reducers/login.js
@@ -2,23 +2,11 @@ import * as actionTypes from '../actions/actionTypes';
 import produce from "immer";
 
 const initialState = {
-    username: '',
-    password: '',
     user: {},
     loading: false,
     isLoggedIn: false,
 }
-const setUsername = (state, action) =>{
-    return produce(state, draft=>{
-        draft.username = action.username
-    })
-}
 
-const setPassword = (state, action) =>{
-    return produce(state, draft=>{
-        draft.password = action.password
-    })
-}
 const setIsLoggedIn = (state, action) =>{
     return produce(state, draft=>{
         draft.isLoggedIn = action.isLoggedIn;
@@ -35,10 +23,6 @@ const setLoggedInUser = (state, action) =>{
 
 const reducer = (state = initialState , action)=>{
     switch (action.type) {
-    case actionTypes.SET_PASSWORD:
-        return setPassword(state, action);
-    case actionTypes.SET_USERNAME:
-        return setUsername(state, action);
     case actionTypes.SET_IS_LOGGED_IN:
         return setIsLoggedIn(state, action);
     case actionTypes.SET_LOGGEDIN_USER:
--- football-betting/src/store/reducers/matchResults.js
+++ football-betting/src/store/reducers/matchResults.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     currentResults: [],
-    
+
     thirteen: 0,
     twelve: 0,
     eleven: 0,
@@ -12,37 +12,32 @@ const initialState = {
     loading: false
 }
 
-const intializeResults=(state , action) =>{
+const intializeResults = (state, action) => {
     return produce(state, draft => {
-        draft.currentResults.splice(draft.currentResults.length, 
-            (draft.currentResults.length+1), action.payload);
+        draft.currentResults.splice(draft.currentResults.length,
+            (draft.currentResults.length + 1), action.payload);
         draft.loading = true;
     });
 }
 
-const setUpWinners=(state, action) =>{
-    return produce(state, draft=>{
-        draft.thirteen = (action.jackpot * action.thirteenPercent)/action.thirteenPieces;
-        draft.twelve = (action.jackpot * action.twelvePercent)/action.twelvePieces;
-        draft.eleven = (action.jackpot * action.elevenPercent)/action.elevenPieces;
-        draft.ten = (action.jackpot * action.tenPercent)/action.tenPieces;
+const setUpWinners = (state, action) => {
+    return produce(state, draft => {
+        draft.thirteen = (action.jackpot * action.thirteenPercent) / action.thirteenPieces;
+        draft.twelve = (action.jackpot * action.twelvePercent) / action.twelvePieces;
+        draft.eleven = (action.jackpot * action.elevenPercent) / action.elevenPieces;
+        draft.ten = (action.jackpot * action.tenPercent) / action.tenPieces;
     });
 }
 
-
-const reducer = (state = initialState , action)=>{
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.INITIALIZE_CURRENT_RESULT:
             return intializeResults(state, action);
         case actionTypes.SETUP_WINNERS:
             return setUpWinners(state, action);
-            default:
+        default:
             return state;
     }
 }
 
-
 export default reducer;
-
-
-
