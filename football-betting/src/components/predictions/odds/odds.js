a7bac600f81813685211b5a30c7aba6527b9123b
lordshegz <lordshegz@gmail.com>
2021-02-02 07:45:32 +0000

    minute bug fixes

--- football-betting/src/components/predictions/odds/odds.js
+++ football-betting/src/components/predictions/odds/odds.js
@@ -4,11 +4,11 @@ import classes from './odds.module.css';
 const odds = (props) =>{
     return (<div className={classes.OddsWrapper}>
 
-        <div className = 'row' >
-            <div className='col-4'>Home(H)</div><div className='col-4'>Draw(D)</div><div className='col-4'>Away(A)</div>
+        <div className = {classes.Titles} >
+            <div className={classes.TitleChild}>Home(H)</div><div className={classes.TitleChild}>Draw(D)</div><div className={classes.TitleChild}>Away(A)</div>
         </div>
-        <div className = 'row' style ={{color: 'grey'}} >
-            <div className='col-4'>{props.home}</div><div className='col-4'>{props.draw}</div><div className='col-4'>{props.away}</div>
+        <div className ={classes.BodyValues}  >
+            <div className={classes.BodyInner}>{props.home}</div><div className={classes.BodyInner} >{props.draw}</div><div className= {classes.BodyInner}>{props.away}</div>
         </div>
     </div>)
 }
