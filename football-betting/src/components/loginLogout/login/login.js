ef85792f0648fb99e04c73a079656ac5c289b78c
lordshegz <lordshegz@gmail.com>
2021-02-02 20:14:55 +0000

    removing disable signup button

--- football-betting/src/components/loginLogout/login/login.js
+++ football-betting/src/components/loginLogout/login/login.js
@@ -2,7 +2,7 @@ import { Button, FormControl, Form } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import React, { useState } from 'react';
 import axios from '../../../axios-users';
-import classes from './login.module.css'; 
+import classes from './login.module.css';
 
 const Login = (props) => {
     const [showPopup, setShowPopUp] = useState(false);
@@ -25,7 +25,7 @@ const Login = (props) => {
         e.preventDefault();
         login().then(data => {
             setData(data);
-            
+
             props.setIsLoggedIn(true);
             popUpFunc();
             props.setLoggedInUser(data);
@@ -52,17 +52,18 @@ const Login = (props) => {
         }, 2000);
         setShowPopUp(true);
     }
-    const handleForgot=()=>{
+    const handleForgot = () => {
         setForgot(false);
         history.push("forgot-password");
     }
+    let alerts = ["alert", "alert-danger"]
+    showPopup ? alerts.push(classes.alertShown) : alerts.push(classes.alertHidden);
     return (<div>
-
         {showPopup ?
             !data.username ?
-                <div className={`alert alert-danger ${showPopup && data.username ? 'alert-shown' : 'alert-hidden'}`}>
+                <div className={alerts.join(" ")}>
                     <strong>Failure!</strong> Please check username or password!
-        </div>
+                </div>
                 : null
             : null
 
@@ -81,10 +82,10 @@ const Login = (props) => {
                 value={username} placeholder="username" className="mr-sm-2" />
             <FormControl type="password" onChange={(e) => setPassword(e.target.value)}
                 value={password} placeholder="password" className="mr-sm-2" />
-                {forgot? 
+            {forgot ?
                 <Button className={classes.Forgot} onClick={handleForgot} >forgot password?</Button>
                 : null}
-            <Button type="submit" variant="outline-light" disabled={username === "" || password === ""}>Login</Button>
+            <Button type="submit" variant="outline-light">Login</Button>
 
             <Button onClick={HandleSignup} variant="outline-light">SignUp</Button>
         </Form>
