4ca18e0d4f7b4414b057e31062b20bab5f94ced0
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-03-05 02:43:48 +0200

    footer

--- football-betting/src/components/UI/LandingPage/landing.jsx
+++ football-betting/src/components/UI/LandingPage/landing.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./landing.module.css";
 import { connect } from "react-redux";
 import CountDown from "../CountDown/CountDown";
+import Footer from "../Footer/Footer";
 class Landing extends Component {
   constructor(props) {
     super(props);
@@ -9,18 +10,12 @@ class Landing extends Component {
       loading: false,
     };
   }
-  componentDidMount = () => {
-    if (!this.state.loading) {
-      setTimeout(() => {
-        console.log("I am in landing page", this.props.gameDateRaw);
-      }, 3000);
-    }
-    this.setState({ loading: true });
-  };
+  
   render() {
     return (
       <div className={classes.LandingWrapper}>
         <CountDown gamedate={this.props.gameDateRaw}/>
+        <Footer/>
       </div>
     );
   }
