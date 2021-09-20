d3090eba546607e9ee3d8658edabb9a2b6b504ff
lordshegz <lordshegz@gmail.com>
2021-09-20 22:19:44 +0100

    payout code

--- src/store/reducers/board.js
+++ src/store/reducers/board.js
@@ -129,6 +129,7 @@ const setReceipt = (state, action) => {
             let user = firebase.auth().currentUser;
             slip.userId = user.uid;
             slip.isEvaluated = false;
+            slip.isPaid = false;
             slip.postponed = false;
             let historyRef = firebase.database().ref("game-history").child(user.uid).child(slip.gameNumber);
             historyRef.set(slip);
